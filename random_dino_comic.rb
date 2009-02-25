34802b3afe45f62f9d9e46645539b96cb0773b27
Edward Ocampo-Gooding <edward@edwardog.net>
2009-02-25 18:38:21 -0500

    Figuring out how to clear image cache

--- random_dino_comic.rb
+++ random_dino_comic.rb
@@ -1,23 +1,18 @@
 Shoes.app do
-  background "#555"
-  
-  stack :margin => 10 do
-    title strong("Random Dinosaur Comic"), :align => "center", :stroke => "#DFA", :margin => 0
-    
-    @comic_placeholder = flow :width => 500 do
-      @temp_comic = image "unicorn.jpg", :width => 50, :height => 50,  :align => "center"
-    end
-
-    @status = caption "Hold on; we’re choosing a random comic...", :align => "center"
+  def random_comic
+    @status.text = "Hold on; we’re choosing a random comic..."
     
     download 'http://www.ohnorobot.com/random.pl?comic=23' do |random_comic_page|
       comic_url = "http://www.qwantz.com/" + random_comic_page.response.body[/(comics\/comic.*?png)/]
       
-      download comic_url, :save => File.basename("recent_comic.png"),
+      # debug(File.exists?("recent_comic.png"))
+      File.delete("recent_comic.png") if File.exists?("recent_comic.png")
+      # debug(File.exists?("recent_comic.png"))
+      
+      # Weird that recent_comic.png does not get overwritten
+      download comic_url, :save => "recent_comic.png",
         :progress => proc { 
           @status.text = "Ok! Pulling it down from the interpipes..."
-          @temp_comic.top += (-20..20).rand
-          @temp_comic.left += (-20..20).rand
         },
         :finish => proc {
           @comic_placeholder.clear
@@ -25,5 +20,19 @@ Shoes.app do
           @status.text = comic_url
         }
     end
+  end
+  
+  background "#555"
+  
+  stack :margin => 10 do
+    title strong("Random Dinosaur Comic"), :align => "center", :stroke => "#DFA", :margin => 0
+    
+    @comic_placeholder = flow :width => 500
+
+    @status = caption "Hold on; we’re choosing a random comic...", :align => "center"
+    
+    random_comic
+    
+    button("Yes, I would like another.") { random_comic }
   end
 end
